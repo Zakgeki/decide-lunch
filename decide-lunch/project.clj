1037813f430e5e2387329dce426daf7fcff05143
Zakgeki <zwells@email.sc.edu>
2020-08-21 14:00:33 -0400

    modified:   decide-lunch/project.clj 	modified:   decide-lunch/target/default/classes/META-INF/maven/decide-lunch/decide-lunch/pom.properties 	modified:   decide-lunch/target/default/stale/leiningen.core.classpath.extract-native-dependencies

--- decide-lunch/project.clj
+++ decide-lunch/project.clj
@@ -1,8 +1,8 @@
 (defproject decide-lunch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A program to determine where to go to lunch using a weighted list"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License (MIT)"
+            :url "https://www.mit-license.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot decide-lunch.core
   :target-path "target/%s"
